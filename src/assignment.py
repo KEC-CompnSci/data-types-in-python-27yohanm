4d60a7a3a8b65ce764167f6020ab379002e31009
27yohanm <27yohanm@retsdstudent.ca>
2025-02-18 20:13:27 +0000

    hi

--- src/assignment.py
+++ src/assignment.py
@@ -9,3 +9,36 @@ word_1 = "robots"
 word_2 = "built"
 word_3 = "today"
 word_4 = "were"
+
+
+
+num_str_1 = "42"
+num_int_1 = int(num_str_1)
+
+num_str_2 = "13" 
+num_int_2 = int(num_str_2)     
+
+num_str_3 = "7" 
+num_int_3 = int(num_str_3)
+
+total_num= num_int_1 + num_int_2
+
+total_num_str= str(total_num)
+
+
+word_1 = " robots"
+word_1_str = str(word_1)
+
+word_2 = " built"
+word_2_str= str(word_2)
+
+
+word_3 = " today"
+word_3_str= str(word_3)
+
+word_4 = " were"
+word_4_str= str(word_4)
+
+sentence= total_num_str + word_1_str  + word_4_str  + word_2_str  + word_3_str
+
+print(sentence)
